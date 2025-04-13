593f98fe20836c5a57748847b93e1d06adeafbe8
Kate Lanman <katelanman@Kates-MacBook-Pro.local>
2025-04-13 08:36:33 -0400

    cnn testing architecture

--- code/cnn.py
+++ code/cnn.py
@@ -8,7 +8,7 @@ from keras.losses import binary_crossentropy
 import pyarrow.feather as feather
 import numpy as np
 
-def fish_cnn(img_rows, img_cols):
+def fish_cnn1(img_rows, img_cols):
     """
     creates CNN for classifying fish images
     params:
@@ -16,7 +16,6 @@ def fish_cnn(img_rows, img_cols):
         img_cols (int): number of cols in the image
     returns: CNN model
     """
-    # TODO: optimize
     inpx = Input(shape=(img_rows, img_cols, 1))
 
     # can also add padding in here with the padding = 'valid' (no padding) or 'same' (padding) option
@@ -42,4 +41,91 @@ def fish_cnn(img_rows, img_cols):
     
     return model
 
+def fish_cnn2(img_rows, img_cols):
+    """
+    creates CNN for classifying fish images
+    params:
+        img_rows (int): number of rows in the image
+        img_cols (int): number of cols in the image
+    returns: CNN model
+    """
+    inpx = Input(shape=(img_rows, img_cols, 1))
 
+    conv1 = Conv2D(1, kernel_size=(3, 3), strides=1, activation=None, padding='same')(inpx)
+    conv2 = Conv2D(1, kernel_size=(3, 3), strides=1, activation=None)(conv1)
+
+    # need to flatten the images for the hidden layer/output
+    flat_G = Flatten()(conv2)
+
+    # can decide how many hidden nodes to use
+    hidden1 = Dense(1000, activation='sigmoid')(flat_G)
+    hidden2 = Dense(500, activation='sigmoid')(hidden1)
+
+    out_layer = Dense(1, activation='sigmoid')(hidden2)
+
+    model = Model([inpx], [out_layer])
+    model.compile(optimizer=SGD(),
+                loss=binary_crossentropy,
+                metrics=['accuracy'])
+    
+    return model
+
+def fish_cnn3(img_rows, img_cols):
+    """
+    creates CNN for classifying fish images
+    params:
+        img_rows (int): number of rows in the image
+        img_cols (int): number of cols in the image
+    returns: CNN model
+    """
+    inpx = Input(shape=(img_rows, img_cols, 1))
+
+    conv1 = Conv2D(1, kernel_size=(3, 3), strides=1, activation=None, padding='same')(inpx)
+    conv2 = Conv2D(1, kernel_size=(3, 3), strides=1, activation=None)(conv1)
+
+    # need to flatten the images for the hidden layer/output
+    flat_G = Flatten()(conv2)
+
+    # can decide how many hidden nodes to use
+    hidden1 = Dense(1000, activation='relu')(flat_G)
+    hidden2 = Dense(500, activation='relu')(hidden1)
+
+    out_layer = Dense(1, activation='sigmoid')(hidden2)
+
+    model = Model([inpx], [out_layer])
+    model.compile(optimizer=SGD(),
+                loss=binary_crossentropy,
+                metrics=['accuracy'])
+    
+    return model
+
+def fish_cnn4(img_rows, img_cols):
+    """
+    creates CNN for classifying fish images
+    params:
+        img_rows (int): number of rows in the image
+        img_cols (int): number of cols in the image
+    returns: CNN model
+    """
+    # TODO: optimize
+    inpx = Input(shape=(img_rows, img_cols, 1))
+
+    conv1 = Conv2D(1, kernel_size=(3, 3), strides=1, activation=None, padding='same')(inpx)
+    conv2 = Conv2D(1, kernel_size=(3, 3), strides=1, activation=None)(conv1)
+
+    # need to flatten the images for the hidden layer/output
+    flat_G = Flatten()(conv2)
+
+    # can decide how many hidden nodes to use
+    hidden1 = Dense(1000, activation='sigmoid')(flat_G)
+    hidden2 = Dense(500, activation='relu')(hidden1)
+    hidden3 = Dense(250, activation='relu')(hidden2)
+
+    out_layer = Dense(1, activation='sigmoid')(hidden3)
+
+    model = Model([inpx], [out_layer])
+    model.compile(optimizer=SGD(),
+                loss=binary_crossentropy,
+                metrics=['accuracy'])
+    
+    return model
